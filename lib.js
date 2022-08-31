f106de866ac9871bcb035cf696b07ab4c6c437b4
Tanner Hornsby <tannerhornsby7@gmail.com>
2022-08-31 01:56:10 -0500

    Configure layout, Add JS for new book insertion

--- lib.js
+++ lib.js
@@ -1,34 +1,88 @@
 let myLibrary = [];
+const library = document.getElementById('library');
+let read = false;
+let i = 0;
 
-function Book(title, author, pages, read) {
-    this.title;
-    this.author;
-    this.pages;
-    this.read;
+// Book object constructor
+function Book(title, author, pages, read, data) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.data = data;
     this.info = function () {
         return title + ", " + author + ", " + pages + ", " + read;
+    };
+}
+
+// read button click manager
+const readyornot = document.getElementById('readyornot');
+
+readyornot.addEventListener('click', () => {
+    read = !read;
+    if(read) {
+        readyornot.classList.add('read');
+        readyornot.textContent = "Read";
     }
-}
-
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-}
-
-const catInHat = new Book("The Cat in the Hat", "Dr. Seuss", 45, false);
-const theTwits = new Book("The Twits", "Roald Dahl", 60, true);
-
-addBookToLibrary(catInHat);
-addBookToLibrary(theTwits);
-
-
-
-function printBooks() {
-    let output = "";
-    for (let i = 0; i < myLibrary.length; i++) {
-        output += "Book #" + (i + 1) + " is: " + myLibrary[i].info() + '\n';
+    else {
+        readyornot.classList.remove('read');
+        readyornot.textContent = "Not Read";
     }
-    return output;
+});
+
+
+// add book to library function
+
+function addBookToLibrary() {
+
+    //form values
+    const inTitle = document.getElementById('title').value;
+    const inAuthor = document.getElementById('author').value;
+    const inPages = document.getElementById('pages').value;
+
+    const book = new Book(inTitle, inAuthor, inPages, read, i);
+
+    let backcover = document.createElement('div');
+    let frontcover = document.createElement('div');
+    let bot = document.createElement('div');
+    let title = document.createElement('h2');
+    let author = document.createElement('h3');
+    let pages = document.createElement('h4');
+
+    backcover.classList.add("backofbook");
+    frontcover.classList.add("bookcover");
+    bot.classList.add("bot");
+
+    console.log(book.title)
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+
+    if(book.read) {
+        backcover.classList.add("backread");
+        frontcover.classList.add("read");
+    }
+
+    frontcover.appendChild(title);
+    bot.appendChild(author);
+    bot.appendChild(pages);
+    frontcover.appendChild(bot);
+    backcover.appendChild(frontcover);
+    library.appendChild(backcover);
+
+    //appending object to array
+    myLibrary.push(book)
+    i++;
 }
 
+//getting form submissions
+const addlib = document.getElementById('addlib');
 
-console.log(printBooks());
+//adding form specified book to library
+addlib.addEventListener('click', () => addBookToLibrary());
+
+
+//remove book logic
+
+//toggle read status logic
+
