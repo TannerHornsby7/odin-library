bf6aa16e3be12fb483c041bd55c259a6ee6c7b3f
Tanner Hornsby <tannerhornsby7@gmail.com>
2022-08-30 19:20:07 -0500

    Create book object and library array

--- lib.js
+++ lib.js
@@ -1,9 +1,34 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+    this.title;
+    this.author;
+    this.pages;
+    this.read;
+    this.info = function () {
+        return title + ", " + author + ", " + pages + ", " + read;
+    }
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(book) {
+    myLibrary.push(book);
 }
+
+const catInHat = new Book("The Cat in the Hat", "Dr. Seuss", 45, false);
+const theTwits = new Book("The Twits", "Roald Dahl", 60, true);
+
+addBookToLibrary(catInHat);
+addBookToLibrary(theTwits);
+
+
+
+function printBooks() {
+    let output = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+        output += "Book #" + (i + 1) + " is: " + myLibrary[i].info() + '\n';
+    }
+    return output;
+}
+
+
+console.log(printBooks());
